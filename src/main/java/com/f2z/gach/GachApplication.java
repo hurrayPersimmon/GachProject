9430f764b9d7dd0afffcbf2e64138a9b7a87e2aa
hurrayPersimmon <rahel337@naver.com>
2024-04-08 09:46:33 +0900

    first commit

---
+++ src/main/java/com/f2z/gach/GachApplication.java
@@ -0,0 +1,13 @@
+package com.f2z.gach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f2z/gach/GachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f2z.gach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
