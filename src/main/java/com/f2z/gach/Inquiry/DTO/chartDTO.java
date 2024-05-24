db9ae24c303778d24aebdf05011f41cf7f371ee4
mxxxjun <mj991212@gachon.ac.kr>
2024-05-25 01:16:53 +0900

    메인 대시보드 수정 - 4

--- src/main/java/com/f2z/gach/Inquiry/DTO/chartDTO.java
+++ src/main/java/com/f2z/gach/Inquiry/DTO/chartDTO.java
@@ -4,12 +4,17 @@ import lombok.*;
 
 import java.time.LocalDate;
 
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @ToString
 public class chartDTO {
     private LocalDate date;
     private Long count;
+
+    public chartDTO(java.sql.Date date, Long count) {
+        this.date = date.toLocalDate();
+        this.count = count;
+    }
 }
