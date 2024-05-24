428ba34f2f378c61f07d3c4fd4ab161ae8e942ea
mxxxjun <mj991212@gachon.ac.kr>
2024-05-25 01:39:17 +0900

    대시보드 수정

--- src/main/java/com/f2z/gach/Inquiry/DTO/chartDTO.java
+++ src/main/java/com/f2z/gach/Inquiry/DTO/chartDTO.java
@@ -13,6 +13,7 @@ public class chartDTO {
     private LocalDate date;
     private Long count;
 
+
     public chartDTO(java.sql.Date date, Long count) {
         this.date = date.toLocalDate();
         this.count = count;
