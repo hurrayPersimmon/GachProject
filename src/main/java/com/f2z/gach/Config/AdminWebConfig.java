2d5478e9b5f6b7a61d883459dcb46b6fb6a6eac4
mxxxjun <mj991212@gachon.ac.kr>
2024-06-06 14:43:35 +0900

    필요없는 파일 삭제

--- src/main/java/com/f2z/gach/Config/AdminWebConfig.java
+++
@@ -1,8 +0,0 @@
-package com.f2z.gach.Config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AdminWebConfig {
-//    AdminRepository adminRepository = new AdminRepository();
-}
